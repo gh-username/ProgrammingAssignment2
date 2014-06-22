2a8e5ceb0d07fb7773850e7f7ead2d62b315c753
gh-username <wintermute47@gmail.com>
2014-06-22 19:15:11 -0400

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions used to create inverse of a matrix and cache the result.
 
-## Write a short comment describing this function
+## the makeCacheMatrix function creates a
+## list containing functions to do the following:
 
-makeCacheMatrix <- function(x = matrix()) {
+## 1.  set the value of the matrix.
+## 2.  get the value of the matrix.
+## 3.  set the value of the matrix inverse.
+## 4.  get the value of the matrix inverse.
 
+makeCacheMatrix <- function(mat = matrix()) {
+        
+        matInv <- NULL ## intialize matrix inverse variable.
+        
+        set <- function(newMat) { ## set the matrix variable.
+                mat <<- newMat
+                matInv <<- NULL
+        }
+        
+        get <- function() {mat} ## get the matrix variable.
+        
+        setInvMat <- function(inv) {matInv <<- inv} ## set the matrix inverse using "<<-" operator to cache the value.
+        
+        getInvMat <- function() {matInv} ## get the matrix inverse.
+        
+        list(set = set, get = get,
+             setInvMat = setInvMat,
+             getInvMat = getInvMat) ## create list of function names.
+        
 }
 
 
-## Write a short comment describing this function
+## this function will return a matrix that is the inverse of the matrix passed
+## as the first argument.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mat, ...) {
+        
+        mI <- mat$getInvMat() ## initialize matrix inverse variable.
+        
+        if(!is.null(mI)) {
+                message("getting cached data")  ## check to see if value is cached and use cached value if available.
+                return(mI)
+        }
+        
+        data <- mat$get() ## get matrix.
+        
+        mI <- solve(data, ...) ## find matrix inverse value.
+        
+        mat$setInvMat(mI) ## assign matrix inverse value to return variable
+        
+        return(mI)
 }
